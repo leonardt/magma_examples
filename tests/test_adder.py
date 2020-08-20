0ee8c5deca3d4df26e3eec3c22774d38c5f0b5e1
Lenny Truong <lenny@stanford.edu>
2020-08-20 16:50:53 -0700

    Remove msg param

--- tests/test_adder.py
+++ tests/test_adder.py
@@ -15,7 +15,7 @@ def test_adder4(N):
     tester.circuit.B = B = BitVector.random(N)
     tester.circuit.CIN = CIN = BitVector.random(1)
     tester.eval()
-    tester.circuit.SUM.expect(A + B + CIN.zext(N - 1), msg=f"N={N}")
+    tester.circuit.SUM.expect(A + B + CIN.zext(N - 1))
     tester.circuit.COUT.expect((A.zext(1) + B.zext(1) +
                                 CIN.zext(N))[-1])
     tester.compile_and_run("verilator")
