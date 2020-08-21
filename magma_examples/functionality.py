3fddf57965c03137612d0a190d1428275870aefc
Lenny Truong <lenny@stanford.edu>
2020-08-21 14:47:31 -0700

    Add functionality example

---
+++ magma_examples/functionality.py
@@ -0,0 +1,15 @@
+import magma as m
+
+
+@m.combinational2()
+def clb(a: m.UInt[16], b: m.UInt[16], c: m.UInt[16], 
+        d: m.UInt[16]) -> m.UInt[16]:
+    return (a & b) | (~c & d)
+
+class Functionality(m.Circuit):
+    io = m.IO(
+        x=m.In(m.UInt[16]),
+        y=m.In(m.UInt[16]),
+        z=m.Out(m.UInt[16]),
+    )
+    io.z @= clb(io.x, io.y, io.x, io.y)
---
+++ tests/test_functionality.py
@@ -0,0 +1,12 @@
+from hwtypes import BitVector
+
+import fault
+from magma_examples.functionality import Functionality, clb
+
+
+def test_functionality():
+    tester = fault.Tester(Functionality)
+    x = BitVector.random(16)
+    y = BitVector.random(16)
+    tester(x, y).expect(clb(x, y, x, y))
+    tester.compile_and_run("verilator")
