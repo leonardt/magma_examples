a8d2d7bd3b559b18ffe68f1de66247c6261dbe31
Lenny Truong <lenny@stanford.edu>
2020-08-21 16:13:01 -0700

    Add gcd

---
+++ magma_examples/gcd.py
@@ -0,0 +1,20 @@
+import magma as m
+
+
+@m.sequential2()
+class GCD:
+    def __init__(self):
+        self.x = m.Register(m.UInt[16])()
+        self.y = m.Register(m.UInt[16])()
+
+    def __call__(self, a: m.In(m.UInt[16]), b: m.In(m.UInt[16]), 
+                 load: m.In(m.Bit)) -> (m.Out(m.UInt[16]), m.Out(m.Bit)):
+        if load:
+            self.x = a
+            self.y = b
+        elif self.y != 0:
+            if self.x > self.y:
+                self.x = self.x - self.y
+            else:
+                self.y = self.y - self.x
+        return self.x.prev(), self.y.prev() == 0
---
+++ tests/test_gcd.py
@@ -0,0 +1,15 @@
+import fault
+from magma_examples.gcd import GCD
+
+
+def test_gcd():
+    tester = fault.SynchronousTester(GCD, clock=GCD.CLK)
+    tester.circuit.a = 32
+    tester.circuit.b = 16
+    tester.circuit.load = 1
+    tester.advance_cycle()
+    tester.circuit.load = 0
+    tester.advance_cycle()
+    tester.wait_on(tester.circuit.O1 == 1)
+    tester.circuit.O0.expect(16)
+    tester.compile_and_run("verilator", flags=['--trace'])
