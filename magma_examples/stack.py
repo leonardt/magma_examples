cf12a71e4120806e7995475c636c4cee4629757d
Lenny Truong <lenny@stanford.edu>
2020-09-01 16:06:05 -0700

    Add stack

---
+++ magma_examples/stack.py
@@ -0,0 +1,36 @@
+import magma as m
+
+
+class Stack(m.Generator2):
+    def __init__(self, depth):
+        self.io = io = m.IO(
+            push=m.In(m.Bit),
+            pop=m.In(m.Bit),
+            en=m.In(m.Bit),
+            data_in=m.In(m.UInt[32]),
+            data_out=m.Out(m.UInt[32]),
+        ) + m.ClockIO()
+
+        stack_mem = m.Memory(depth, m.UInt[32])()
+        stack_pointer = m.Register(m.UInt[m.bitutils.clog2(depth + 1)])()
+        out_reg = m.Register(m.UInt[32])()
+
+        wen = io.en & io.push & (stack_pointer.O < depth)
+        stack_mem.WE @= wen
+        stack_mem.WDATA @= io.data_in
+        stack_mem.WADDR @= stack_pointer.O[:-1]
+
+        stack_mem.RADDR @= stack_pointer.O[:-1] - 1
+
+        @m.inline_combinational()
+        def logic():
+            stack_pointer.I @= stack_pointer.O
+            if wen:
+                stack_pointer.I @= stack_pointer.O + 1
+            elif io.en & io.pop & (stack_pointer.O > 0):
+                stack_pointer.I @= stack_pointer.O - 1
+
+            out_reg.I @= out_reg.O
+            if stack_pointer.O > 0:
+                out_reg.I @= stack_mem.RDATA
+        io.data_out @= out_reg.O
---
+++ tests/test_stack.py
@@ -0,0 +1,74 @@
+from hwtypes import BitVector
+import fault
+
+from magma_examples.stack import Stack
+
+
+def push(tester, data):
+    tester.circuit.en = 1
+    tester.circuit.data_in =  data
+    tester.circuit.push = 1
+    tester.advance_cycle()
+    tester.circuit.en = 0
+    tester.circuit.data_in = 0
+    tester.circuit.push = 0
+
+
+def expect_pop(tester, data):
+    tester.circuit.en = 1
+    tester.circuit.pop = 1
+    tester.advance_cycle()
+    tester.circuit.en = 0
+    tester.circuit.pop = 0
+    tester.circuit.data_out.expect(data)
+
+
+def push_and_pop(tester, data):
+    tester.circuit.en = 1
+    tester.circuit.data_in =  data
+    tester.circuit.push = 1
+    tester.circuit.pop = 1
+    tester.advance_cycle()
+    tester.circuit.en = 0
+    tester.circuit.data_in = 0
+    tester.circuit.push = 0
+    tester.circuit.pop = 0
+
+
+def test_stack():
+    Stack4 = Stack(4)
+    tester = fault.SynchronousTester(Stack4, Stack4.CLK)
+    push(tester, 3)
+    push(tester, 1)
+    push(tester, 2)
+    push(tester, 0)
+    expect_pop(tester, 0)
+    expect_pop(tester, 2)
+    expect_pop(tester, 1)
+    expect_pop(tester, 3)
+
+    # Extra pops shouldn't change value
+    expect_pop(tester, 3)
+    expect_pop(tester, 3)
+
+    push(tester, 3)
+
+    # pushing and popping should only push
+    push_and_pop(tester, 2)
+    push_and_pop(tester, 3)
+    expect_pop(tester, 3)
+    expect_pop(tester, 2)
+    expect_pop(tester, 3)
+
+    # pushing more than depth will be ignored
+    push(tester, 0)
+    push(tester, 1)
+    push(tester, 2)
+    push(tester, 3)
+    push(tester, 4)
+    expect_pop(tester, 3)
+    expect_pop(tester, 2)
+    expect_pop(tester, 1)
+    expect_pop(tester, 0)
+
+    tester.compile_and_run("verilator")
