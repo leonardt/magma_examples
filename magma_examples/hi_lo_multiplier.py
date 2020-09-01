841f708b994f5a4ca13dfdf181d3eb0624d00e38
Lenny Truong <lenny@stanford.edu>
2020-09-01 08:43:03 -0700

    Add comment

--- magma_examples/hi_lo_multiplier.py
+++ magma_examples/hi_lo_multiplier.py
@@ -2,6 +2,9 @@ import magma as m
 
 
 class HiLoMultiplier(m.Circuit):
+    """
+    A 16*16-bit multiplier with separate high and low product outputs
+    """
     io = m.IO(A=m.In(m.UInt[16]), B=m.In(m.UInt[16]),
               Hi=m.Out(m.UInt[16]), Lo=m.Out(m.UInt[16]))
     mult = m.zext(io.A, 16) * m.zext(io.B, 16)
