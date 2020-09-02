4d28bd20e4ec36ed1ca7ec33ecf9d6c8a2856a22
Lenny Truong <lenny@stanford.edu>
2020-09-02 16:12:01 -0700

    Add reset_shift_register

---
+++ magma_examples/reset_shift_register.py
@@ -0,0 +1,13 @@
+import magma as m
+
+
+class ResetShiftRegister(m.Circuit):
+    io = m.IO(
+        I=m.In(m.UInt[4]),
+        shift=m.In(m.Bit),
+        O=m.Out(m.UInt[4])
+    ) + m.ClockIO(has_resetn=True)
+    regs = [m.Register(m.UInt[4], has_enable=True, reset_type=m.ResetN)()
+            for _ in range(4)]
+    io.O @= m.fold(regs, foldargs={"I": "O"})(io.I, CE=io.shift,
+                                              RESETN=io.RESETN)
---
+++ tests/test_reset_shift_register.py
@@ -0,0 +1,31 @@
+import fault
+
+from magma_examples.reset_shift_register import ResetShiftRegister
+
+
+def test_reset_shift_register():
+    tester = fault.SynchronousTester(ResetShiftRegister,
+                                     clock=ResetShiftRegister.CLK)
+    seq = [0, 1, 1, 0, 1, 0, 0, 1]
+    delay = [0, 0, 0]
+    in_seq, out_seq = seq + delay, delay + seq
+    tester.circuit.shift = 1
+    for i, o in zip(in_seq, out_seq):
+        tester.circuit.I = i
+        tester.advance_cycle()
+        tester.circuit.O.expect(o)
+    tester.circuit.shift = 0
+    tester.circuit.I = 1
+    for i in range(4):
+        tester.advance_cycle()
+        tester.circuit.O.expect(1)
+    tester.circuit.RESETN = 1
+    tester.advance_cycle()
+    tester.circuit.RESETN = 0
+    tester.advance_cycle()
+    tester.circuit.RESETN = 1
+    tester.circuit.shift = 1
+    for i in range(4):
+        tester.advance_cycle()
+        tester.circuit.O.expect(0)
+    tester.compile_and_run("verilator")
