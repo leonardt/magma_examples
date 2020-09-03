b43b522edaeb580d35c145d677364c42181f2eab
Lenny Truong <lenny@stanford.edu>
2020-09-03 09:16:29 -0700

    Add risc example

---
+++ magma_examples/risc.py
@@ -0,0 +1,48 @@
+import magma as m
+from mantle import RegFileBuilder
+
+
+class Risc(m.Circuit):
+    io = m.IO(
+        is_write=m.In(m.Bit),
+        write_addr=m.In(m.UInt[8]),
+        write_data=m.In(m.UInt[32]),
+        boot=m.In(m.Bit),
+        valid=m.Out(m.Bit),
+        out=m.Out(m.UInt[32])
+    ) + m.ClockIO(has_async_reset=True)
+    file = RegFileBuilder("file", 256, 32, write_forward=False)
+    code = RegFileBuilder("code", 256, 32, write_forward=False)
+    pc = m.Register(m.UInt[8])()
+
+    instr = code[pc.O]
+
+    op = instr[24:]
+    rci = instr[16:24]
+    rai = instr[8:16]
+    rbi = instr[:8]
+
+    ra = m.mux([file[rai], 0], rai == 0)
+    rb = m.mux([file[rbi], 0], rbi == 0)
+
+    code.write(io.write_addr, io.write_data, enable=io.is_write)
+
+    @m.inline_combinational()
+    def logic():
+        rc = m.Bits[32](0)
+        io.valid @= False
+        if io.is_write:
+            pc.I @= pc.O
+        elif io.boot:
+            pc.I @= 0
+        else:
+            if op == 0:
+                rc = ra + rb
+            elif op == 1:
+                rc = m.zext((rai << 8) | rbi, 24)
+            if rci == 255:
+                io.valid @= True
+            pc.I @= pc.O + 1
+
+    io.out @= rc
+    file.write(rci, rc, enable=(rci != 255))
---
+++ magma_examples/test_risc.py
@@ -0,0 +1,36 @@
+import fault
+
+from magma_examples.risc import Risc
+
+
+def test_risc():
+    tester = fault.SynchronousTester(Risc, Risc.CLK)
+
+    def write(addr, data):
+        tester.circuit.is_write = 1
+        tester.circuit.write_addr = addr
+        tester.circuit.write_data = data
+        tester.advance_cycle()
+
+    def boot():
+        tester.circuit.is_write = 0
+        tester.circuit.boot = 1
+        tester.advance_cycle()
+
+    def I(op, rc, ra, rb):
+        return (op << 24) | (rc << 16) | (ra << 8) | rb
+
+    app = [
+        I(1, 1, 0, 1),  # r1 <- 1
+        I(0, 1, 1, 1),  # r1 <- r1 + r1
+        I(0, 1, 1, 1),  # r1 <- r1 + r1
+        I(0, 255, 1, 0),  # rh <- r1
+    ]
+    write(0, 0)
+    for addr, instr in enumerate(app):
+        write(addr, instr)
+    boot()
+    tester.circuit.boot = 0
+    tester.wait_until_high(tester.circuit.valid)
+    tester.circuit.out.expect(4)
+    tester.compile_and_run("verilator", magma_opts={"verilator_compat": True})
