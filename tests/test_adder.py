24c09cbe5f1bc973d1a79f96df7864b1d08b2ccf
Lenny Truong <leonardtruong@fb.com>
2022-11-02 13:53:06 -0700

    Test MLIR output

--- tests/test_adder.py
+++ tests/test_adder.py
@@ -19,4 +19,5 @@ def test_adder(N):
     tester.circuit.COUT.expect((A.zext(1) + B.zext(1) +
                                 CIN.zext(N))[-1])
     with tempfile.TemporaryDirectory() as dir_:
-        tester.compile_and_run("verilator", directory=dir_)
+        tester.compile_and_run("verilator", directory=dir_,
+                               magma_output="mlir-verilog")
